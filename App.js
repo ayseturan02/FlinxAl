c461d5c6cde30f69299040fcc17e01e42b7c6711
AYŞE TURAN <ayse0025turan@gmail.com>
2025-02-04 13:24:00 +0300

    fonts

---
+++ App.js
@@ -0,0 +1,16 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{fontSize: 50, fontFamily: 'HammersmithOneRegular'}}>
+        App
+      </Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
---
+++ src/screens/EnterSc/index.js
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const EnterSc = () => {
+  return (
+    <View>
+      <Text>EnterSc</Text>
+    </View>
+  );
+};
+
+export default EnterSc;
+
+const styles = StyleSheet.create({});
