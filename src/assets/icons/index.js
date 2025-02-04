baa93a017a39f4a505047de436e065ba9c7fd6bc
AYŞE TURAN <ayse0025turan@gmail.com>
2025-02-04 12:47:38 +0300

    first commit

---
+++ src/assets/icons/index.js
@@ -0,0 +1,49 @@
+const Back = require('./Back.png');
+const ContactUs = require('./ContactUs.png');
+const Customer = require('./Customer.png');
+const Home = require('./Home.png');
+const InfoSquared = require('./InfoSquared.png');
+const MembershipCard = require('./MembershipCard.png');
+const Menu = require('./Menu.png');
+const MoonSymbol = require('./MoonSymbol.png');
+const MoreInfo = require('./MoreInfo.png');
+const OpenInBrowser = require('./OpenInBrowser.png');
+const PlusMath = require('./PlusMath.png');
+const Plus = require('./Plus.png');
+const PushNotification = require('./PushNotification.png');
+const QuestionMark = require('./QuestionMark.png');
+const Search = require('./Search.png');
+const SparklingDiamond = require('./SparklingDiamond.png');
+const SparklingDiamondWhite = require('./SparklingDiamondWhite.png');
+const TestAccount = require('./TestAccount.png');
+const TestAccount1 = require('./TestAccount1.png');
+const ThumbsUpDown = require('./ThumbsUpDown.png');
+const Unavailable = require('./Unavailable.png');
+const UserShield = require('./UserShield.png');
+const WiFiOff = require('./WiFiOff.png');
+
+export {
+  Back,
+  ContactUs,
+  Customer,
+  Home,
+  InfoSquared,
+  MembershipCard,
+  Menu,
+  MoonSymbol,
+  MoreInfo,
+  OpenInBrowser,
+  PlusMath,
+  Plus,
+  PushNotification,
+  QuestionMark,
+  Search,
+  SparklingDiamond,
+  SparklingDiamondWhite,
+  TestAccount,
+  TestAccount1,
+  ThumbsUpDown,
+  Unavailable,
+  UserShield,
+  WiFiOff,
+};
---
+++ src/assets/images/index.js
@@ -0,0 +1,4 @@
+const tempImage = require('./tempImage.png');
+const tempImage1 = require('./tempImage1.png');
+const tempImage2 = require('./tempImage2.png');
+export {tempImage, tempImage1, tempImage2};
